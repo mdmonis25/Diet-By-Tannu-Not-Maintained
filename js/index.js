fa1fc780eb227390b87a378b9e78373c203d4072
mdmonis <mdmonis128@gmail.com>
2024-03-13 13:26:01 +0530

    Email Sender Initialized

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var http = require('http');
+var path = require('path')
+var nodemailer = require('nodemailer');
+
+var app = express();
+var server = http.Server(app);
+var port = 500;
+
+app.set("port",port);
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"../index.html")))
+
+
