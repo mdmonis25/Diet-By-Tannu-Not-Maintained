f1c28a02cb107088a6e662b18a77d7177614ccb2
mdmonis <mdmonis128@gmail.com>
2024-03-13 14:42:09 +0530

    Styling working properly

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var http = require('http');
+var path = require('path')
+var nodemailer = require('nodemailer');
+
+var app = express();
+var server = http.Server(app);
+var port = 500;
+
+app.set("port",port);
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "")));
+app.use(express.static(path.join(__dirname,"./index.html")))
+
+//Routing
+
+app.get("/", function(req,response){
+    response.sendFile(path.join(__dirname, "./index.html"))
+})
+
+//Initializing Server
+server.listen(port, function(){
+    console.log("Starting Server on port " + port)
+})
